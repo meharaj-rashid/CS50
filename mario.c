e5511369fee92c30129b97ffbe2896c766c79939
mechraj <94051997+mechraj@users.noreply.github.com>
2023-10-15 16:48:44 +0400

    Adds a do whilw loop to check for descrepencies from user

--- mario.c
+++ mario.c
@@ -4,7 +4,13 @@
 
 int main(void)
 {
-    int n=get_int("Size: ");
+   int n;
+   do
+   {
+    n=get_int("size: ");
+   }
+   while (n<1);
+    
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
