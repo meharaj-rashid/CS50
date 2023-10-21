85525239a8b9752119fe96e076abee679636b666
mechraj <94051997+mechraj@users.noreply.github.com>
2023-10-21 21:00:40 +0400

    Adds get_size and print_size function

--- mario.c
+++ mario.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
-#include<cs50.h>
+#include <cs50.h>
 #include<cs50.c>
+
+int get_size(void);
+void print_size(int n);
+
+
 
 int main(void)
 {
-   int n;
-   do
-   {
-    n=get_int("size: ");
-   }
-   while (n<1);
-    
+    // Get size of grid
+    int n=get_size();
+
+    // Prints the size of grid
+    print_size(n);
+}
+
+int get_size(void)
+{
+    int n;
+    do
+    {
+        n=get_int("Size: ");
+    }
+    while(n<0);
+    return n;
+
+}
+   
+void print_size(int n)
+{
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
@@ -20,3 +39,4 @@ int main(void)
         printf("\n");
     }
 }
+   
